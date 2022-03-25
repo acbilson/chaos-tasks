86f83256bfe7e6a71bb388f53ecb9ba841b7a3cf
Alex Bilson <acbilson@gmail.com>
2022-03-25 08:36:25 -0500

    shifts project from Blazor Server to Golang

--- src/Models/TodoTask.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChaosTasks.Models;
-public class Todo
-{
-  public string Raw { get; set; }
-  public string Text { get; set; }
-  public string Priority { get; set; }
-  public string[] Projects { get; set; }
-  public string[] Contexts { get; set; }
-}
---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module chaos-tasks/core/v2
+
+go 1.17
